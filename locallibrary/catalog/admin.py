624155486a6330ee80b28e3020f52ab49e68b4ec
tonytit98 <tonytit15598@gmail.com>
2020-08-07 16:26:11 +0700

    Done maybe =))

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -23,14 +23,12 @@ class BookAdmin(admin.ModelAdmin):
 # admin.site.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'date_of_birth', 'date_of_death')
-    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
     fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
     inlines = [BooksInline]
 
 
 # admin.site.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'display_genre')
     list_display = ('book', 'status', 'due_back', 'id')
     list_filter = ('status', 'due_back')
     fieldsets = (
