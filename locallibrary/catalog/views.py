de02ad563387ce78aa2419283b8064e2d5fb50f0
tonytit98 <tonytit15598@gmail.com>
2020-08-10 21:15:00 +0700

    add part6

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from catalog.models import Book, Author, BookInstance, Genre
+from django.views import generic
 
 def index(request):
     """View function for home page of site."""
@@ -23,3 +24,23 @@ def index(request):
     
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
+
+
+class BookListView(generic.ListView):
+    model = Book
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get the context
+        context = super(BookListView, self).get_context_data(**kwargs)
+        # Create any data and add it to the context
+        context['some_data'] = 'This is just some data'
+        return context
+
+
+class BookDetailView(generic.ListView):
+    model = Book
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get the context
+        context = super(BookDetailView, self).get_context_data(**kwargs)
+        # Create any data and add it to the context
+        context['some_data'] = 'This is just some data'
+        return context
