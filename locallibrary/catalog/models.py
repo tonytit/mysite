2ca645eddd8da1a3f2087035d7ca33fe82998a2c
tonytit <20162912@student.hust.edu.vn>
2020-08-11 10:43:38 +0700

    Django Tutorial Part 5: Creating our home page

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -6,14 +6,11 @@ from django.urls import reverse  # To generate URLS by reversing URL patterns
 
 
 class Genre(models.Model):
-    """Model representing a book genre (e.g. Science Fiction, Non Fiction)."""
+    """Model representing a book genre."""
     name = models.CharField(
-        max_length=200,
-        help_text="Enter a book genre (e.g. Science Fiction, French Poetry etc.)"
-        )
-
+        max_length=200, help_text='Enter a book genre (e.g. Science Fiction)')
     def __str__(self):
-        """String for representing the Model object (in Admin site etc.)"""
+        """String for representing the Model object."""
         return self.name
 
 
