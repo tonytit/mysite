c27e7ccdbc62029a05f81324b90987c017331882
tonytit <20162912@student.hust.edu.vn>
2020-08-12 14:37:33 +0700

    Django Tutorial Part 9: Working with forms

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
     path('authors/', views.AuthorListView.as_view(), name='authors'),
     path('author/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail'),
+<<<<<<< HEAD
     path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
     path('borrowed/',views.AllLoanedBooksListView.as_view(), name='borrowed'),
     path('book/<uuid:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian'),
@@ -16,4 +17,6 @@ urlpatterns = [
     path('book/create/', views.BookCreate.as_view(), name='book_create'),
     path('book/<int:pk>/update/', views.BookUpdate.as_view(), name='book_update'),
     path('book/<int:pk>/delete/', views.BookDelete.as_view(), name='book_delete'),
+=======
+>>>>>>> Django Tutorial Part 6: Generic list and detail views
 ]
