d7b95538d1ac5c0e5b6289d2fd6adbeabc5fe2ac
tonytit <20162912@student.hust.edu.vn>
2020-08-13 13:39:35 +0700

    Django Tutorial Part 10: Testing a Django web application

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -100,13 +100,14 @@ class AllLoanedBooksListView (LoginRequiredMixin,generic.ListView):
 
 @permission_required('catalog.can_mark_returned')
 def renew_book_librarian(request, pk):
+    """View function for renewing a specific BookInstance by librarian."""
     book_instance = get_object_or_404(BookInstance, pk=pk)
 
     # If this is a POST request then process the Form data
     if request.method == 'POST':
 
         # Create a form instance and populate it with data from the request (binding):
-        form = RenewBookForm(request.POST)
+        book_renewal_form = RenewBookForm(request.POST)
 
         # Check if the form is valid:
         if form.is_valid():
@@ -115,15 +116,15 @@ def renew_book_librarian(request, pk):
             book_instance.save()
 
             # redirect to a new URL:
-            return HttpResponseRedirect(reverse('all-borrowed') )
+            return HttpResponseRedirect(reverse('all-borrowed'))
 
-    # If this is a GET (or any other method) create the default form.
+    # If this is a GET (or any other method) create the default form
     else:
         proposed_renewal_date = datetime.date.today() + datetime.timedelta(weeks=3)
-        form = RenewBookForm(initial={'renewal_date': proposed_renewal_date})
+        book_renewal_form = RenewBookForm(initial={'renewal_date': proposed_renewal_date})
 
     context = {
-        'form': form,
+        'book_renewal_form': book_renewal_form,
         'book_instance': book_instance,
     }
 
